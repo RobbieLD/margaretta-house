62194d904a389ab51983cafd51ed650289a3b374
Rob Davis <bfe5e925@opayq.com>
2024-06-23 13:47:21 +0100

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <div class="soon">Coming Soon</div>
+  </main>
+</template>
+
+<style scoped>
+    .soon {
+        height: 100vh;
+        width: 100vw;
+        display: grid;
+        justify-content: center;
+        align-content: center;
+        font-size: 1.5em;
+        font-family: Calibri;
+    }
+</style>
